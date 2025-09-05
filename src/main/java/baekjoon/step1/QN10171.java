47e11b2477ad72fc008dbaf1f1aa027c96ccc710
ParkYoungDuk <dydejrdms@naver.com>
2025-09-05 11:19:07 +0900

    백준 10171 고양이

---
+++ src/main/java/baekjoon/step1/QN10171.java
@@ -0,0 +1,11 @@
+package baekjoon.step1;
+
+public class QN10171 {
+    public static void main(String[] args) {
+        // 익스케이프 문자를 고려하여 진행하도록 출제된 문제
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
