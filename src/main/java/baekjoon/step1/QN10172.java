38b0e0462e90fb11dd11559b970762e968b352be
ParkYoungDuk <dydejrdms@naver.com>
2025-09-05 11:25:25 +0900

    백준 10172 개

---
+++ src/main/java/baekjoon/step1/QN10172.java
@@ -0,0 +1,12 @@
+package baekjoon.step1;
+
+public class QN10172 {
+    public static void main(String[] args){
+        // 익스케이프 문자와 큰따옴표 처리 문제
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
