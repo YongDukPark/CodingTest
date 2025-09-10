0a46383c2c475ca279acb0ededac5c5fd54bed08
ParkYoungDuk <dydejrdms@naver.com>
2025-09-10 14:47:46 +0900

    백준 15552 빠른 A+B

--- src/main/java/baekjoon/step3/QN15552_빠른_A더하기B.java
+++ src/main/java/baekjoon/step3/QN15552_빠른_A더하기B.java
@@ -26,27 +26,5 @@ public class QN15552_빠른_A더하기B {
         } catch(IOException io){
             System.err.print(io);
         }
-
-//        기존 코드
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        HashMap<Integer, Integer[]> map = new HashMap<Integer, Integer[]>();
-//        ArrayList<Integer> answer = new ArrayList<Integer>();
-//        try {
-//            int cnt = Integer.valueOf(br.readLine());
-//            for (int i = 1; i <= cnt; i++) {
-//                map.put(i, Arrays.stream(br.readLine().split(" ")).map(Integer::valueOf).toArray(Integer[]::new));
-//            }
-//
-//            for (int i = 1; i <= cnt; i++) {
-//                bw.write(String.valueOf(map.get(i)[0] + map.get(i)[1]));
-//                if (i != cnt) {
-//                    bw.write("\n");
-//                }
-//            }
-//            bw.flush();
-//        } catch(IOException io){
-//            System.err.print(io);
-//        }
     }
 }
