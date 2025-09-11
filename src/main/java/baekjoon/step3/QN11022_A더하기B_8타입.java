29cb54c9279c3310546a091bd349b69933b79c97
ParkYoungDuk <dydejrdms@naver.com>
2025-09-11 18:12:01 +0900

    백준 11022 A+B - 8

---
+++ src/main/java/baekjoon/step3/QN11022_A더하기B_8타입.java
@@ -0,0 +1,34 @@
+package baekjoon.step3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class QN11022_A더하기B_8타입{
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        try{
+            int count = Integer.valueOf(br.readLine());
+
+            for(int i = 0 ; i < count ; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.valueOf(st.nextToken());
+                int b = Integer.valueOf(st.nextToken());
+                sb.append("Case #").append(i+1).append(": ")
+                        .append(a).append(" + ").append(b).append(" = ")
+                        .append(a+b).append("\n");
+            }
+
+            bw.write(sb.toString());
+            bw.flush();
+        } catch(IOException io){
+            System.err.println(io);
+        }
+    }
+}
