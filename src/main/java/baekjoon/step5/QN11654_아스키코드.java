12df74c67b61d77ee5daba193209a78de4aa4770
ParkYoungDuk <dydejrdms@naver.com>
2025-09-17 13:41:20 +0900

    백준 11654 아스키코드

---
+++ src/main/java/baekjoon/step5/QN11654_아스키코드.java
@@ -0,0 +1,12 @@
+package baekjoon.step5;
+
+import java.util.Scanner;
+
+public class QN11654_아스키코드 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char text = sc.nextLine().charAt(0);
+
+        System.out.println((int)text);
+    }
+}
