ca5202b448dd475a62a227397658695201ffd80d
ParkYoungDuk <dydejrdms@naver.com>
2025-09-17 13:20:09 +0900

    백준 2743 단어 길이 재기

---
+++ src/main/java/baekjoon/step5/QN2743_단어_길이_재기.java
@@ -0,0 +1,11 @@
+package baekjoon.step5;
+
+import java.util.Scanner;
+
+public class QN2743_단어_길이_재기 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        System.out.println(text.length());
+    }
+}
